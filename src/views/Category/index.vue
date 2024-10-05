f818bdde00065c0dce8d786240e73763ace95834
yuanzia-liu <2523632216@qq.com>
2024-10-06 00:51:55 +0800

    home轮播图实现

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted, onUpdated } from 'vue'
 import { getCategoryAPI } from '@/apis/category'
+import { getBannerAPI } from '@/apis/home'
 import { useRoute } from 'vue-router'
 
 // 获取数据
@@ -12,6 +13,17 @@ const getCateGory = async () => {
 }
 onMounted(() => getCateGory())
 onUpdated(() => getCateGory())
+
+// 获取banner
+const bannerList  = ref([])
+
+const getBanner = async () => {
+  const res = await getBannerAPI({
+    distributionSite: '2'
+  })
+  bannerList.value = res.result
+}
+onMounted(() => {getBanner()})
 </script>
 
 <template>
@@ -24,6 +36,14 @@ onUpdated(() => getCateGory())
           <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+      <!-- 轮播图 -->
+      <div class="home-banner">
+    <el-carousel height="500px">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
+      </el-carousel-item>
+    </el-carousel>
+  </div>
     </div>
   </div>
 </template>
@@ -105,6 +125,16 @@ onUpdated(() => getCateGory())
 
   .bread-container {
     padding: 25px 0;
+  }
+}
+.home-banner {
+  width: 1240px;
+  height: 500px;
+  margin: 0 auto;
+
+  img {
+    width: 100%;
+    height: 500px;
   }
 }
 </style>
