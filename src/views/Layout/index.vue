775d745f13a070532c0a6aecd1bfca4627063fb9
yuanzia-liu <2523632216@qq.com>
2024-10-05 15:08:51 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,16 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
   <div>首页
     <!-- 二级路由出口 -->
-    <router-view></router-view>
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
   </div>
   
 </template>  
