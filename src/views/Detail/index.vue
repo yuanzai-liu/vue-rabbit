aae2d957dec2fd5b856613ab51b34e00c6292829
yuanzia-liu <2523632216@qq.com>
2024-10-07 00:42:14 +0800

    登录-表单校验

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -42,7 +42,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures" />
+              <XtxImageView :image-list="goods.mainPictures" />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
